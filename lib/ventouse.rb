5c5fde729cfa0927cc410bd2b226f6fb62dc473f
Ilia Ablamonov <ilia@flamefork.ru>
2009-04-30 13:07:34 +0500

    Ventouse.random_pronouncable_password

--- lib/ventouse.rb
+++ lib/ventouse.rb
@@ -14,4 +14,16 @@ class Ventouse
   def self.disable_rails_transactions
     require 'ventouse/disable_transactions'
   end
+
+  #http://snippets.dzone.com/posts/show/2137
+  def self.random_pronouncable_password(size = 6)
+    c = %w(b c d f g h j k l m n p qu r s t v w x z ch cr fr nd ng nk nt ph pr rd sh sl sp st th tr)
+    v = %w(a e i o u y)
+    f, r = true, ''
+    (size * 2).times do
+      r << (f ? c[rand * c.size] : v[rand * v.size])
+      f = !f
+    end
+    r
+  end
 end
