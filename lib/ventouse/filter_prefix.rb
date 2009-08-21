330ff99a7d063c58a70c664a3b1a9cf676293e3a
Ivan Efremov <loki@work00.(none)>
2009-08-21 15:34:56 +0500

    String filter_prefix! method added

--- lib/ventouse/filter_prefix.rb
+++ lib/ventouse/filter_prefix.rb
@@ -2,4 +2,9 @@ class String
   def filter_prefix prefix
     self.starts_with?(prefix) ? self[prefix.to_s.length, self.length] : self
   end
+
+  def filter_prefix! prefix
+    self.slice! Regexp.new "^#{prefix}"
+  end
+
 end
