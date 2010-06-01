05c1d1ed15752ddfd19f3736d4022cff0fa23c54
Striker <strikeroff@gmail.com>
2010-06-01 11:55:36 +0400

    Filter Prefix

--- lib/ventouse/filter_prefix.rb
+++ lib/ventouse/filter_prefix.rb
@@ -1,12 +1,12 @@
 class String
-  def filter_prefix! prefix
-    sliced = slice! /^#{prefix}/
+  def filter_prefix! prefix, ignore_case = false
+    sliced = ignore_case ? slice!(/^#{prefix}/i) : slice!(/^#{prefix}/)
     !sliced.nil?
   end
 
-  def filter_prefix prefix
+  def filter_prefix prefix, ignore_case = false
     s = clone
-    s.filter_prefix! prefix
+    s.filter_prefix! prefix, ignore_case
     s
   end
 end
